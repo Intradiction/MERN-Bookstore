aeff8b40aa5c6ca2ffc6f3504ac73adcfa6b3451
InnovationTT <greylyngao@gmail.com>
2024-01-11 13:07:36 -0500

    handle CORS policy

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDBURL } from "./config.js";
-import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/bookRoutes.js";
 
 const app = express();
-app.use(express.json());
-app.use('/books', booksRoute);
 
+// Middleware for parsing JSON request body
+app.use(express.json());
+
+// Middleware for handling CORS policy
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  }
+));
+
+app.use('/books', booksRoute);
 
 // Connect to MongoDB, then if connection successful, start express server
 mongoose
