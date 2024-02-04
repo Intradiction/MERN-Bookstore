572f0bb1541903ba0d931224c3547de079fc52ab
InnovationTT <greylyngao@gmail.com>
2024-02-03 20:39:28 -0500

    update cors

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,13 @@ app.use(express.json());
 
 // Middleware for handling CORS policy
 app.use(
-  cors()
-  // cors({
-  //   origin: 'http://localhost:3000',
-  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  //   allowedHeaders: ['Content-Type'],
-  // })
+  //cors()
+  cors({
+    origin: ['http://localhost:3000', 'https://mern-bookstore-frontend-eight.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+    //allowedHeaders: ['Content-Type'],
+  })
 );
 
 app.use('/books', booksRoute);
