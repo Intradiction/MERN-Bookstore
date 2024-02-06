785b052327d3dfc7f695b5a35889c59495f1d89c
InnovationTT <greylyngao@gmail.com>
2024-02-05 21:07:38 -0500

    oh it was the cors in index.js that was *

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(
   //cors()
   cors({
-    origin: '*', //['https://mern-bookstore-frontend-eight.vercel.app']
+    origin: ['https://mern-bookstore-frontend-eight.vercel.app'],
     methods: '*', //['GET', 'POST', 'PUT', 'DELETE']
     allowedHeaders: ['Content-Type'],
     credentials: true,
